aaab463076542efcaed4c0c0b4d214c4c2eaa51f
Rohit Mehta <rmehta.bitspilani@gmail.com>
2022-08-24 19:40:55 +0530

    Update EDA_with_plotly.py

--- EDA_with_plotly.py
+++ EDA_with_plotly.py
@@ -4,5 +4,51 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 
+#How to get cancer dataset from sklearn?
 
+#Here reading from pre-shared csv
+cancer_df = pd.read_csv('cancer.csv')
+cancer_df.head()
 
+#tail can be used to check the number of datapoints in the dataframe
+cancer_df.tail()
+
+## Boxplots ##
+#To plot a boxplot you can use
+sns.boxplot(x=df['target'],y=df['mean area'])
+#Boxplot can be used to find outliers
+
+#Equivalent of this in plotly express is
+fig = px.box(cancer_df,x='target',y='mean area')
+fig.show()
+
+#To see all the points in the boxplot itself
+fig = px.box(cancer_df,x='target',y='mean area',points='all')
+fig.show()
+
+## Histogram ##
+#How to get nbins for histogram?
+fig = px.histogram(cancer_df,x='mean radius',nbins=60)
+fig.show()
+
+#To add a target parameter also to the histogram
+fig = px.histogram(cancer_df,x='mean radius',color='target',nbins=60)
+fig.show()
+#Very powerful to get initial insights for the target variable
+
+#Histogram plots with marginal plots
+
+#Marginal plot of type 'rug'
+px.histogram(cancer_df,x='mean radius',color='target',marginal='rug',nbins=60).show()
+
+#Marginal plot of type 'box'- Can check Q1,Q3, upper fence and lower fence for the target variables
+px.histogram(cancer_df,x='mean radius',color='target',marginal='box',nbins=60).show()
+
+#Marginal plot of type 'violin'- Can check Q1,Q3, upper fence and lower fence for the target variables
+px.histogram(cancer_df,x='mean radius',color='target',marginal='violin',nbins=60).show()
+
+## Interactive Density HeatMap
+#It is a 2D visualization of a histogram
+px.density_heatmap(cancer_df,x='mean radius',y='mean texture')
+
+## 
